4d3234c377d535669236de053db81807b8530228
Swifty-sketch <marsel.davletov@chasacademy.se>
2023-11-07 12:57:57 +0100

    updated version

--- pizzabot.js
+++ pizzabot.js
@@ -2,30 +2,31 @@ const vegetarian = "Vegetarian Pizza";
 const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
 
-let pizzaPrice = 80;
+const pizzaPrice = 80;
+
 let orderQuantity;
 let orderName;
 let orderTotal;
 let inputElement = document.getElementById("svarInput");
 
-function svarBtn() {
+function checkOrderName() {
     orderName = inputElement.value;
-    inputElement.value = "";
+    inputElement.value = ""; // Tar bort texten
 
-    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
-        document.getElementById("btn1").style.display = "none";
-        document.getElementById("btn2").style.display = "inline";
+    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) { // Kollar igenom ifall orderName är en av pizzorna
+        document.getElementById("btn1").style.display = "none"; // Tar bort "checkOrderName" btn
+        document.getElementById("btn2").style.display = "inline"; // Lägger till "fulFillOrder" btn, så att andra function kan köras.
         document.getElementById("placeHolder").innerHTML = "How many of " + orderName + " do you want?";
     } else {
         document.getElementById("placeHolder").innerHTML = "You have misspelled the chosen pizza, retry. You can choose between Vegetarian Pizza, Hawaiian Pizza, and Pepperoni Pizza";
-        svarBtn()
     }
 }
 
-function svarBtn2() {
+function fulFillOrder() { // Räknar ut värdet + skriver ut resultatet.
     orderQuantity = inputElement.value;
     orderQuantity = parseInt(orderQuantity);
     orderTotal = orderQuantity * pizzaPrice;
+
     document.getElementById("placeHolder").innerHTML = "Great, I'll get started on your " + orderName + " right away. It will cost " + orderTotal + "kr. " + calculateCookTime(orderQuantity);
 }
 
